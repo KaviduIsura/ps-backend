66c2ed3dfe5d67c16baf1fbbd4c92b889a784c19
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-02 02:06:23 +0530

    create user model file ✅📝

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("users", userSchema);
+export default User;
