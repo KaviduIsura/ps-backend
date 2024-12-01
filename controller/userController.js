f20f362864f14b314882f05d4e9596d5f3bceec9
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-02 02:07:09 +0530

    create user controller file and save user function 💾✅

---
+++ controller/userController.js
@@ -0,0 +1,19 @@
+import User from "../models/User.js";
+import bcrypt, { compareSync } from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export async function createUser(req, res) {
+  try {
+    const newUserData = req.body;
+    newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+    const user = new User(newUserData);
+    await user.save();
+    res.json({
+      message: "User Created",
+    });
+  } catch (error) {
+    res.json({
+      message: error.message,
+    });
+  }
+}
