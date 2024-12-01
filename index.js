df9b3e583fb51d05105fc774e21d6107271928ab
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-02 01:18:59 +0530

    Connect database 📊✅

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, {});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Database connected");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
