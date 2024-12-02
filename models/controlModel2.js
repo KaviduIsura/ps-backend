ea2928fcd66bacbb682db33cda6abc11d7dc4521
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-02 21:15:16 +0530

    Create model for acuator details in Db 🕹️📊

---
+++ models/controlModel2.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const controlSchema2 = new mongoose.Schema(
+  {
+    valve: { type: Boolean, default: false },
+    led: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Control2 = mongoose.model("Control2", controlSchema2);
+
+export default Control2;
