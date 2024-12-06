6941f81953a88babdba8e931a11af79d8457ab7c
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-06 21:52:49 +0530

    Change the server port ⏭️

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ connection.once("open", () => {
 // Routes
 app.use("/api", router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5003;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
