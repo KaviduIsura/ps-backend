e40e8b7ba7bfbf2bc6d147b11a45c64a0b8af9e7
Kavidu Isura <144512406+KaviduIsura@users.noreply.github.com>
2024-12-07 11:14:41 +0530

    Add mannual controle for this project 🕹️✅

--- controller/controlController.js
+++ controller/controlController.js
@@ -5,7 +5,15 @@ export const getControlStates = async (req, res) => {
   try {
     const control = await Control.findOne().sort({ createdAt: -1 }); // Get the latest record
     if (!control) {
-      return res.status(200).json({ fan1: false, fan2: false, led: false }); // Default state
+      return res.status(200).json({
+        fan1: false,
+        fan2: false,
+        led: false,
+        manual: false,
+        manualFan1: false,
+        manualFan2: false,
+        manualLed: false,
+      }); // Default state
     }
     res.status(200).json(control);
   } catch (error) {
@@ -16,8 +24,16 @@ export const getControlStates = async (req, res) => {
 // Update the control states
 export const updateControlStates = async (req, res) => {
   try {
-    const { fan1, fan2, led } = req.body;
-    const newControl = new Control({ fan1, fan2, led });
+    const { fan1, fan2, led, manual, manualFan1, manualFan2, manualLed } = req.body;
+    const newControl = new Control({
+      fan1,
+      fan2,
+      led,
+      manual,
+      manualFan1,
+      manualFan2,
+      manualLed,
+    });
     await newControl.save();
     res
       .status(200)
--- models/controlModel.js
+++ models/controlModel.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
-//get stautes of thermometer, humidity and light
+
 const controlSchema = new mongoose.Schema(
   {
     fan1: { type: Boolean, default: false },
     fan2: { type: Boolean, default: false },
     led: { type: Boolean, default: false },
+    manual: { type: Boolean, default: false }, // Manual mode flag
+    manualFan1: { type: Boolean, default: false },
+    manualFan2: { type: Boolean, default: false },
+    manualLed: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
